07825116553ed2e6a9dd6866645b24257c8859f4
kuzmin <kuz_alex_konst@mail.ru>
2024-03-31 21:58:55 +0400

    Initial

---
+++ src/main/java/ru/kuzmin/conveyer/ConveyerApplication.java
@@ -0,0 +1,13 @@
+package ru.kuzmin.conveyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConveyerApplication.class, args);
+	}
+
+}
